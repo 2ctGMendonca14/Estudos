e631a020737a4056caed12e3fd09c4715f4a8c57
2ctGMendonca14 <00001138394968sp@al.educacao.sp.gov.br>
2024-11-08 11:44:43 -0300

    Add files via upload

---
+++ minha ferramenta de estudos/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Clash Royale?',
+    'É um jogo de estratégia muito famoso hoje em dia'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Argentina?',
+    'A capital da Argentina é Buenos Aires'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Pro Evolution Soccer? ',
+    'É um jogo de futebol podendo criar seu time ou seu jogador'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
